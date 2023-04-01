cf85ca980650511b34f4ca447065fa26c62d2cc6
David Moon <hrmoon99@gmail.com>
2023-04-01 06:37:33 -0500

    1.0.0

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export const Contact = () => {
     return (
-        <div id="contact" className="text-center px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 mb-52 contact_section">
+        <div id="contact" className="text-center px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 mb-20 contact_section">
             <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
                 <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
                     <a className="mx-auto">
