a985c969d059b1352cf2d7a903268d6b451e551f
David Moon <hrmoon99@gmail.com>
2023-04-04 17:02:14 -0500

    1.0.0

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,7 +9,7 @@ const NotFoundPage: React.FC<PageProps> = () => {
           <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
             <span className="sr-only">Error</span>404
           </h2>
-          <p className="text-2xl font-semibold md:text-3xl mb-3 min-w-full">Sorry, we couldn't find this page.</p>
+          <p className="text-2xl font-semibold md:text-3xl mb-8 max-w-full">Sorry, we couldn't find this page.</p>
           <a rel="noopener noreferrer" href="https://davidmoon.io/" className="border text-white bg-black hover:bg-white hover:text-black hover:border-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Back to homepage</a>
         </div>
       </div>
